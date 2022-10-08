f534db601620e7f273ccca58c9a96c773a955fe1
bang-ucok <115239015+bang-ucok@users.noreply.github.com>
2022-10-09 04:02:23 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -19,6 +19,7 @@ let wibu = `https://i.ibb.co/n8YcnVX/IMG-20221004-WA0012.jpg`
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://Instagram.com/mhrj34",
+    mediaUrl: "https://www.paypal.com/",                                                                                         
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
     title: 'AL BOT Whatsapp',
